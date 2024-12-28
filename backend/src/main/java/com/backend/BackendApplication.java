93cd15ef4655af9811163fb3d79d5e1198bd6331
Julian Duru <durutheguru@gmail.com>
2024-12-28 17:00:11 +0100

    - CI updates

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -7,7 +7,63 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("MyAtoi: " + Solution.myAtoi("   -042"));
+		// SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
+
+
+class Solution {
+    public static int myAtoi(String s) {
+        int number = 0;
+        int polarity = 1;
+
+        char c, x;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (c == ' ') {
+                if (number > 0) {
+                    break;
+                }
+                continue;
+            }
+            if (c == '+') {
+                if (number > 0) {
+                    break;
+                }
+                continue;
+            }
+            if (c == '-') {
+                if (number > 0) {
+                    break;
+                }
+                polarity = -1;
+                continue;
+            }
+
+            if (c >= '0' && c <= '9') {
+                number *= 10;
+				number = number + (polarity * Character.getNumericValue(c));
+            }
+            else {
+                break;
+            }
+
+            if (i < s.length() - 1) {
+                x = s.charAt(i + 1);
+                if (x >= '0' && x <= '9') {
+                    if (number <= Integer.MIN_VALUE / 10 && x >= Integer.MIN_VALUE % 10) {
+                        return Integer.MIN_VALUE;
+                    }
+                    if (number >= Integer.MAX_VALUE / 10 && x >= Integer.MAX_VALUE % 10) {
+                        return Integer.MAX_VALUE;
+                    }
+                }
+            }
+        }
+
+        return number;
+    }
+}
+
